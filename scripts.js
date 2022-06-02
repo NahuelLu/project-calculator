8a58c1a8e1377ee85897deba62b32dc645b2a082
NahuelLu <nahuellucana1999@gmail.com>
2022-06-02 17:30:45 -0300

    Work calculator only with two operands and one operator

--- scripts.js
+++ scripts.js
@@ -20,11 +20,48 @@ function operate(operator,num,num1){
 function display(){
     const numberContainer = document.querySelector('.numbers-container');
     const buttonsNumber= numberContainer.querySelectorAll('button');
-    buttonsNumber.forEach(button => button.addEventListener("click",displayNumbers));
+    const operatorsContainer = document.querySelector('.operators-container');
+    const buttonsOperators=operatorsContainer.querySelectorAll('.operator');
+    const equalsButton= document.querySelector('#equals');
+    //Events Listeners
+    buttonsNumber.forEach(button => button.addEventListener("click",displayNumber));
+    buttonsOperators.forEach(button=> button.addEventListener('click',displayOperator));
+    equalsButton.addEventListener('click',showResults);
 }
-function displayNumbers(){
+function updateDisplay(content){
     const display=document.querySelector(".display");
-    display.textContent+=this.value;
-    number+=this.value;
+    display.textContent+=content;
 }
+function displayNumber(){
+    updateDisplay(this.value);
+    currentDisplay+=this.value;
+}
+function displayOperator(){
+    currentNumbers.push(currentDisplay);
+    updateDisplay(this.value);
+    currentDisplay="";
+    currentOperator=this.value;
+}
+function showResults(){
+    const display=document.querySelector(".display");
+    currentNumbers.push(currentDisplay);
+    display.textContent=results();
+}
+let currentDisplay="";
+let currentNumbers=[];
+let currentOperator="";
+function results(){
+    console.log(currentNumbers[0]);
+    console.log(currentNumbers[1]);
+    if(currentOperator==="+"){
+        return operate(add,currentNumbers[0],currentNumbers[1]);
+    }else if(currentOperator==="-"){
+        return operate(subtract,currentNumbers[0],currentNumbers[1]);
+    }else if(currentOperator==="*"){
+        return operate(multiply,currentNumbers[0],currentNumbers[1]);
+    }else{
+        return operate(divide,currentNumbers[0],currentNumbers[1]);
+    }
+}
+
 display();
