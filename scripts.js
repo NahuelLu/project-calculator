51a9e272919ff193d13db40d2d11194ff60537cf
NahuelLu <nahuellucana1999@gmail.com>
2022-06-02 15:30:46 -0300

    Add display functions

--- scripts.js
+++ scripts.js
@@ -17,3 +17,14 @@ function divide(num,num1){
 function operate(operator,num,num1){
     return operator(num,num1);
 }
+function display(){
+    const numberContainer = document.querySelector('.numbers-container');
+    const buttonsNumber= numberContainer.querySelectorAll('button');
+    buttonsNumber.forEach(button => button.addEventListener("click",displayNumbers));
+}
+function displayNumbers(){
+    const display=document.querySelector(".display");
+    display.textContent+=this.value;
+    number+=this.value;
+}
+display();
