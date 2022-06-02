a02d369e38a75cad009e8b74df9b44cee5719977
NahuelLu <nahuellucana1999@gmail.com>
2022-06-01 21:48:13 -0300

    Add multiply and divide operation

--- scripts.js
+++ scripts.js
@@ -6,3 +6,11 @@ function add(){
 function subtract(num,num1){
     return num - num1;
 }
+function multiply(){
+    return [...arguments].reduce((total,number) => {
+        return total *number;
+    },1)
+}
+function divide(num,num1){
+    return num/num1;
+}
