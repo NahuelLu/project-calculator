6065e715f69f56fb5e4137cf648e83f7d7df9b02
NahuelLu <nahuellucana1999@gmail.com>
2022-06-05 00:13:26 -0300

    Order code and fix some bugs

--- scripts.js
+++ scripts.js
@@ -1,15 +1,33 @@
+let currentDisplay="";
+let currentNumbers=[];
+let currentOperator="";
+let result="";
+
+const display=document.querySelector(".display");
+const numberContainer = document.querySelector('.numbers-container');
+const buttonsNumber= numberContainer.querySelectorAll('button');
+const operatorsContainer = document.querySelector('.operators-container');
+const buttonsOperators=operatorsContainer.querySelectorAll('.operator');
+const equalsButton= document.querySelector('#equals');
+const clearButton= document.querySelector('#clear');
+const decimalButton= document.querySelector('#decimal-point');
+const undoButton= document.querySelector('#undo');
+
+buttonsNumber.forEach(button => button.addEventListener("click",displayNumber));
+buttonsOperators.forEach(button=> button.addEventListener('click',selectOperator));
+equalsButton.addEventListener('click',showResults);
+clearButton.addEventListener('click',clearCalculator);
+decimalButton.addEventListener('click',displayDecimal);
+undoButton.addEventListener('click',undoNumber);
+
 function add(){
-    return [...arguments].reduce((total,number) => {
-        return total + number;
-    },0)
+    return num + num1;
 }
 function subtract(num,num1){
     return num - num1;
 }
-function multiply(){
-    return [...arguments].reduce((total,number) => {
-        return total *number;
-    },1)
+function multiply(num,num1){
+    return num*num1;
 }
 function divide(num,num1){
     return num/num1;
@@ -17,19 +35,24 @@ function divide(num,num1){
 function operate(operator,num,num1){
     return operator(num,num1);
 }
-//Functions for DOM
-function display(){
-    const numberContainer = document.querySelector('.numbers-container');
-    const buttonsNumber= numberContainer.querySelectorAll('button');
-    const operatorsContainer = document.querySelector('.operators-container');
-    const buttonsOperators=operatorsContainer.querySelectorAll('.operator');
-    const equalsButton= document.querySelector('#equals');
-    const clearButton= document.querySelector('#clear');
-    //Events Listeners
-    buttonsNumber.forEach(button => button.addEventListener("click",displayNumber));
-    buttonsOperators.forEach(button=> button.addEventListener('click',selectOperator));
-    equalsButton.addEventListener('click',showResults);
-    clearButton.addEventListener('click',clearCalculator);
+function undoNumber(){
+    toString(display.textContent).slice(0, -1);
+}
+function displayDecimal(){
+    updateDisplay(".");
+    currentDisplay+=this.value;
+}
+function updateDisplay(content){
+    if(content===""){
+        display.textContent=content;
+    }
+    display.textContent+=content;
+}
+function updateOperator(operator){
+    currentOperator=operator;
+}
+function updateCurrentNumbers(number){
+    currentNumbers.push(number);
 }
 function clearCurrentDisplay(){
     currentDisplay="";
@@ -40,23 +63,14 @@ function clearCalculator(){
     currentNumbers=[];
     currentOperator="";
 }
-function updateDisplay(content){
-    const display=document.querySelector(".display");
-    if(content===""){
-        display.textContent=content;
-    }
-    display.textContent+=content;
-}
 function displayNumber(){
+    if(currentOperator==="" && currentNumbers.length !==0)
+        clearCalculator();
+
     updateDisplay(this.value);
     currentDisplay+=this.value;
 }
-function updateOperator(operator){
-    currentOperator=operator;
-}
-function updateCurrentNumbers(number){
-    currentNumbers.push(number);
-}
+
 function selectOperator(){
     updateCurrentNumbers(currentDisplay);
     updateDisplay(this.value);
@@ -64,31 +78,37 @@ function selectOperator(){
     updateOperator(this.value);
 }
 function showResults(){
-    const display=document.querySelector(".display");
-    currentNumbers.push(currentDisplay);
-    updateDisplayWithResults();
+    if(currentDisplay!==""){
+        updateCurrentNumbers(currentDisplay);
+        updateDisplayWithResults();
+    }
 }
 function updateDisplayWithResults(){
-    const display=document.querySelector(".display");
-    if(currentNumbers.length===2){
+    if(currentNumbers.length===2 && currentOperator!==""){
         display.textContent=results();
         currentDisplay=results();
         currentNumbers=[];
-    }
-}
-let currentDisplay="";
-let currentNumbers=[];
-let currentOperator="";
-function results(){
-    if(currentOperator==="+"){
-        return operate(add,parseInt(currentNumbers[0]),parseInt(currentNumbers[1]));
-    }else if(currentOperator==="-"){
-        return operate(subtract,currentNumbers[0],currentNumbers[1]);
-    }else if(currentOperator==="*"){
-        return operate(multiply,currentNumbers[0],currentNumbers[1]);
     }else{
-        return operate(divide,currentNumbers[0],currentNumbers[1]);
+        currentNumbers=[];
     }
 }
-
-display();
+function displayError(message){
+    alert(message);
+}
+function results(){
+    let number1=Number(currentNumbers[0]);
+    let number2=Number(currentNumbers[1]);
+    if(currentOperator==="+"){
+        return operate(add,number1,number2);
+    }else if(currentOperator==="-"){
+        return operate(subtract,number1,number2);
+    }else if(currentOperator==="*"){
+        return operate(multiply,number1,number2);
+    }else{
+        if(number2==0){
+            displayError("You can divide with 0 dummy!!");
+            return 0;
+        }
+        return operate(divide,number1,number2);
+    }
+}
