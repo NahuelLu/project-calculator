79e22c7140fee2a64443791978b8ad2541acdcd8
NahuelLu <nahuellucana1999@gmail.com>
2022-07-29 12:13:16 -0300

    Refactor structure of js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,114 @@
-let currentDisplay="";
-let currentNumbers=[];
-let currentOperator="";
-let result="";
+"use strict"
+const calculator = (()=>{
+    let currentDisplay="";
+    let currentNumbers=[];
+    let currentOperator="";
+    const updateDisplay=(content)=>{
+        if(content==="") display.textContent=content;
+        display.textContent+=content;
+    }
+    const updateOperator=(operator)=>{
+        currentOperator=operator;
+    }
+    const clearCurrentDisplay=()=>{
+        currentDisplay="";
+    }
+    const _updateCurrentNumbers=(number)=>{
+        currentNumbers.push(number);
+    }
+    const _updateDisplayWithResults=()=>{
+        if(currentNumbers.length===2 && currentOperator!==""){
+            display.textContent=_results();
+            currentDisplay=_results();
+            currentNumbers=[];
+        }else{
+            currentNumbers=[];
+        }
+    }
+    const _add = (num, num1) => num +num1;
+    const _subtract = (num,num1) => num - num1;
+    const _multiply = (num,num1) => num * num1;
+    const _divide = (num,num1) => num / num1;
+    const _operate = (operator,num,num1) => operator(num,num1);
+    const _results=()=>{
+        let number1=Number(currentNumbers[0]);
+        let number2=Number(currentNumbers[1]);
+        switch(currentOperator){
+            case "+":
+                return _operate(_add,number1,number2);
+            case "-":
+                return _operate(_subtract,number1,number2);
+            case "*":
+                return _operate(_multiply,number1,number2);
+            case "/":
+                return _operate(_divide,number1,number2);
+            default:
+                break;
+        }
+    }
+    const clearCalculator = ()=>{
+        updateDisplay("");
+        clearCurrentDisplay();
+        currentNumbers=[];
+        currentOperator="";
+    }
+    const showResults = ()=>{
+        if(currentDisplay!==""){
+            _updateCurrentNumbers(currentDisplay);
+            _updateDisplayWithResults();
+        }
+    }
+    const selectOperator = (operator)=> {
+        _updateCurrentNumbers(currentDisplay);
+        updateDisplay(operator);
+        clearCurrentDisplay();
+        updateOperator(operator);
+    }
+    const displayNumber= (number)=>{
+        if(currentOperator==="" && currentNumbers.length !==0) clearCalculator();
+        updateDisplay(number);
+        currentDisplay+=number;
+    }
+    const displayDecimal = (decimal)=>{
+        updateDisplay(decimal);
+        currentDisplay+=decimal;
+    }
+    const undoNumber= ()=> {
+        display.textContent=display.textContent.slice(0, -1);
+        currentDisplay=currentDisplay.slice(0,-1);
+    }
+    return {
+        clearCalculator,
+        showResults,
+        selectOperator,
+        displayNumber,
+        displayDecimal,
+        undoNumber
+    }
+})();
 
 const display=document.querySelector(".display");
-const numberContainer = document.querySelector('.numbers-container');
-const buttonsNumber= numberContainer.querySelectorAll('button');
-const operatorsContainer = document.querySelector('.operators-container');
-const buttonsOperators=operatorsContainer.querySelectorAll('.operator');
+const buttonsNumber= document.querySelectorAll('.number');
+const buttonsOperators=document.querySelectorAll('.operator');
 const equalsButton= document.querySelector('#equals');
 const clearButton= document.querySelector('#clear');
 const decimalButton= document.querySelector('#decimal-point');
 const undoButton= document.querySelector('#undo');
 
-buttonsNumber.forEach(button => button.addEventListener('click',() => displayNumber(button.value)));
-buttonsOperators.forEach(button => button.addEventListener('click',() => selectOperator(button.value)));
-equalsButton.addEventListener('click',showResults);
-clearButton.addEventListener('click',clearCalculator);
-decimalButton.addEventListener('click',()=> displayDecimal(decimalButton.value));
-undoButton.addEventListener('click',undoNumber);
+buttonsNumber.forEach(button => button.addEventListener('click',() => calculator.displayNumber(button.value)));
+buttonsOperators.forEach(button => button.addEventListener('click',() => calculator.selectOperator(button.value)));
+equalsButton.addEventListener('click',calculator.showResults);
+clearButton.addEventListener('click',calculator.clearCalculator);
+decimalButton.addEventListener('click',()=> calculator.displayDecimal(decimalButton.value));
+undoButton.addEventListener('click',calculator.undoNumber);
 window.addEventListener('keydown',ev => pressKey(ev.key));
 function pressKey(key){
-    if(!isNaN(key)) displayNumber(key);
-    if(isOperator(key)) selectOperator(key);
+    if(!isNaN(key)) calculator.displayNumber(key);
+    if(isOperator(key)) calculator.selectOperator(key);
     //Also i can do this with switch statement for avoiding create more functions
-    if(isUndo(key)) undoNumber();
-    if(isEquals(key)) showResults();
-    if(isDecimal(key)) displayDecimal(key);
+    if(isUndo(key)) calculator.undoNumber();
+    if(isEquals(key)) calculator.showResults();
+    if(isDecimal(key)) calculator.displayDecimal(key);
 }
 function isOperator(key){
     return key==="/" || key==="*" || key==="+" || key==="-" ;
@@ -39,90 +121,4 @@ function isEquals(key){
 }
 function isUndo(key){
     return key==="Backspace";
-}
-function add(){
-    return num + num1;
-}
-function subtract(num,num1){
-    return num - num1;
-}
-function multiply(num,num1){
-    return (num*num1).toFixed(2);
-}
-function divide(num,num1){
-    return (num/num1).toFixed(2);
-}
-function operate(operator,num,num1){
-    return operator(num,num1);
-}
-function undoNumber(){
-    display.textContent=display.textContent.slice(0, -1);
-    currentDisplay=currentDisplay.slice(0,-1);
-}
-function displayDecimal(decimal){
-    updateDisplay(decimal);
-    currentDisplay+=decimal;
-}
-function updateDisplay(content){
-    if(content==="") display.textContent=content;
-    display.textContent+=content;
-}
-function updateOperator(operator){
-    currentOperator=operator;
-}
-function updateCurrentNumbers(number){
-    currentNumbers.push(number);
-}
-function clearCurrentDisplay(){
-    currentDisplay="";
-}
-function clearCalculator(){
-    updateDisplay("");
-    clearCurrentDisplay();
-    currentNumbers=[];
-    currentOperator="";
-}
-function displayNumber(number){
-    if(currentOperator==="" && currentNumbers.length !==0) clearCalculator();
-    updateDisplay(number);
-    currentDisplay+=number;
-}
-
-function selectOperator(operator){
-    updateCurrentNumbers(currentDisplay);
-    updateDisplay(operator);
-    clearCurrentDisplay();
-    updateOperator(operator);
-}
-function showResults(){
-    if(currentDisplay!==""){
-        updateCurrentNumbers(currentDisplay);
-        updateDisplayWithResults();
-    }
-}
-function updateDisplayWithResults(){
-    if(currentNumbers.length===2 && currentOperator!==""){
-        display.textContent=results();
-        currentDisplay=results();
-        currentNumbers=[];
-    }else{
-        currentNumbers=[];
-    }
-}
-function results(){
-    let number1=Number(currentNumbers[0]);
-    let number2=Number(currentNumbers[1]);
-    switch (currentOperator) {
-        case "+":
-            return operate(add,number1,number2);
-        case "-":
-            return operate(subtract,number1,number2);
-        case "*":
-            return operate(multiply,number1,number2);
-        case "/":
-            return operate(divide,number1,number2);
-        default:
-            break;
-    }
-    
 }
